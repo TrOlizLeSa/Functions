cd86ca4d5330f34ee8a325787681f745b3bd0195
TrOlizLeSa <jkee1@vk.com>
2023-04-11 23:08:26 +0300

    Update 9-signals.js

--- Functions/solutions/9-signals.js
+++ Functions/solutions/9-signals.js
@@ -1,10 +1,10 @@
      import get from 'lodash/get.js';
 
 const freeEmailDomains = [
-  'gmail.com',
-  'yandex.ru',
-  'hotmail.com',
-  'yahoo.com',
+  "gmail.com",
+  "yandex.ru",
+  "hotmail.com",
+  "yahoo.com",
 ];
 
 // BEGIN
